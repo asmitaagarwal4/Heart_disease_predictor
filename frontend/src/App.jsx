6fc93974db44613c271ceb0dc12de3bbfdfaac21
Yadubir <yadubirjha2022@vitbhopal.ac.in>
2025-02-25 23:34:23 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react";
 import './App.css'
 import Page from './Components/Page';
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+import Signin from './Components/Signin';
+import Signup from './Components/Signup';
 
 function App() {
   return (
     <div>
-      <Page />
+      <Routes>
+        <Route path="/" element={<Page />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
