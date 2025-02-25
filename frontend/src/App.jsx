8e92e38431ed00d4eadec620fb7000b921637a39
Yadubir <yadubirjha2022@vitbhopal.ac.in>
2025-02-25 23:45:54 +0530

    fixed a bug in routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,18 @@
 import React from "react";
-import './App.css'
-import Page from './Components/Page';
-import { BrowserRouter as Routes, Route } from "react-router-dom";
-import Signin from './Components/Signin';
-import Signup from './Components/Signup';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Page from "./Components/Page";
+import Signin from "./Components/Signin";
+import Signup from "./Components/Signup";
 
 function App() {
   return (
-    <div>
       <Routes>
         <Route path="/" element={<Page />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-    </div>
   );
 }
 
 export default App;
-
